ab9ff6a1240a37474dba83f05aa8fb4e45b9e4b0
Georges034302 <georges.boughantous@gmail.com>
2022-12-22 11:08:32 +1100

    playerapp

---
+++ src/main/java/com/playerapp/PlayerappApplication.java
@@ -0,0 +1,13 @@
+package com.playerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playerapp/PlayerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
